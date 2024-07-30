986e2b1271c0205c8dabef8c2d5694524a3b8a09
Matthew Davidson <matthew@modulolotus.net>
2024-07-30 11:33:43 +0000

    feat: Change 'No results found' display

--- genfact_demo/main.py
+++ genfact_demo/main.py
@@ -30,6 +30,22 @@ log_http_output()
 genfact_server = '34.44.35.203'
 genfact_server_port = 8888
 
+physician_fields = [
+    "first",
+	"last",
+    "specialty", 
+    "npi",
+    "school",
+	"degree"
+    ]
+business_fields = [
+    "legal_name",
+    "addr",
+    "addr2",
+    "city",
+    "zip",
+    ]
+
 templates = Jinja2Blocks(directory="src")
 query_counter = count(1)
 
@@ -88,6 +104,8 @@ async def post_genfact_query(request: Request, english_query: Annotated[str, For
              },
             block_name="query2")
 
+
+
     
 @app.post("/post_pclean_query")
 async def post_pclean_query(request: Request, 
@@ -110,23 +128,30 @@ async def post_pclean_query(request: Request,
         # log.debug(f"pclean response: {pclean_resp}")
 
         extracted_results = extract_docs_and_biz(pclean_resp)
+        docs, biz, doc_keys, biz_keys = extracted_results.values()
+        doc_keys = physician_fields + list(doc_keys - set(physician_fields))
+        biz_keys = business_fields + list(biz_keys - set(business_fields))
         
-        combined = pclean_resp["results"]
-        combined.sort(key=lambda x: x["count"], reverse=True)
-        normalize_counts(combined)
+        # combined = pclean_resp["results"]
+        # combined.sort(key=lambda x: x["count"], reverse=True)
+        # normalize_counts(combined)
 
         sample_count = pclean_resp["count"]
-        not_found_pval = (sample_count - entities_count(combined)) / sample_count
+        not_found_pval = 1 # (sample_count - entities_count(combined)) / sample_count
 
         return templates.TemplateResponse(
             "genfact.html.jinja",
             {"request": request, 
             "idnum": next(query_counter),
+            "physician_fields": physician_fields,
+            "business_fields": business_fields,
             "english_query": english_query,
             "genfact_entity": genfact_entity,
-            "combined": combined,
             "not_found_pval": not_found_pval,
-            **extracted_results,
+            "docs": docs,
+            "biz": biz,
+            "doc_keys": doc_keys,
+            "biz_keys": biz_keys,
             },
             block_name="plot")
     
@@ -142,6 +167,32 @@ async def post_pclean_query(request: Request,
              },
             block_name="plot")
 
+def pclean_row_response(*, pclean_resp: dict, request, english_query, genfact_entity):
+    # log.debug(f"pclean response: {pclean_resp}")
+
+    extracted_results = extract_docs_and_biz(pclean_resp)
+    docs, biz, doc_keys, biz_keys = extracted_results.values()
+    doc_keys = physician_fields + list(doc_keys - set(physician_fields))
+    biz_keys = business_fields + list(biz_keys - set(business_fields))
+    
+    sample_count = pclean_resp["count"]
+    not_found_pval = 1 # (sample_count - entities_count(combined)) / sample_count
+
+    return templates.TemplateResponse(
+        "genfact.html.jinja",
+        {"request": request, 
+        "idnum": next(query_counter),
+        "physician_fields": physician_fields,
+        "business_fields": business_fields,
+        "english_query": english_query,
+        "genfact_entity": genfact_entity,
+        "not_found_pval": not_found_pval,
+        "docs": docs,
+        "biz": biz,
+        "doc_keys": doc_keys,
+        "biz_keys": biz_keys,
+        },
+        block_name="plot")
 
 def extract_docs_and_biz(pclean_result: dict) -> dict:
     docs = {}
@@ -192,3 +243,282 @@ def normalize_counts(entities: list) -> None:
     log.debug(f"total entities count: {total_count}")
     for ent in entities:
         ent["proportion"] = ent["count"] / total_count
+
+
+
+@app.post("/post_pclean_dummy")
+async def post_pclean_dummy(request: Request, empty: bool = True):
+    if empty:
+        pclean_resp = {
+            "businesses_count": 1000,
+            "business_histogram": {},
+            "physician_new_entity": 1000,
+            "physicians": [],
+            "businesses": [],
+            "count": 1000,
+            "business_new_entity": 1000,
+            "results": [],
+            "physician_count": 1000,
+            "physician_histogram": {}
+        }
+    else:
+        pclean_resp = {
+            "businesses_count": 1000,
+            "business_histogram": {
+                "row_47052": 9,
+                "row_45941": 9,
+                "row_47027": 9,
+                "row_44323": 16,
+                "row_47572": 8,
+                "row_45389": 8,
+                "row_48339": 11,
+                "row_46760": 7
+            },
+            "physician_new_entity": 0,
+            "physicians": [{
+                "id": "row_6197",
+                "count": 1000,
+                "entity": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                }
+            }],
+            "businesses": [{
+                "id": "row_46760",
+                "count": 7,
+                "entity": {
+                    "addr": "126 W CHURCH ST",
+                    "addr2": "",
+                    "city": "DILLSBURG",
+                    "zip": "170191280",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_48339",
+                "count": 11,
+                "entity": {
+                    "addr": "126 W CHURCH ST",
+                    "addr2": "SUITE 200",
+                    "city": "DILLSBURG",
+                    "zip": "170191280",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_47052",
+                "count": 9,
+                "entity": {
+                    "addr": "20 CAPITAL DR",
+                    "addr2": "",
+                    "city": "HARRISBURG",
+                    "zip": "171109446",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_44323",
+                "count": 16,
+                "entity": {
+                    "addr": "429 N 21ST ST",
+                    "addr2": "",
+                    "city": "CAMP HILL",
+                    "zip": "170112202",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_47572",
+                "count": 8,
+                "entity": {
+                    "addr": "4665 E TRINDLE RD",
+                    "addr2": "",
+                    "city": "MECHANICSBURG",
+                    "zip": "170503640",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_45389",
+                "count": 8,
+                "entity": {
+                    "addr": "431 N 21ST ST",
+                    "addr2": "",
+                    "city": "CAMP HILL",
+                    "zip": "170112202",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_45941",
+                "count": 9,
+                "entity": {
+                    "addr": "1211 FORGE RD",
+                    "addr2": "",
+                    "city": "CARLISLE",
+                    "zip": "170133183",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "id": "row_47027",
+                "count": 9,
+                "entity": {
+                    "addr": "2313 OAKFIELD RD",
+                    "addr2": "",
+                    "city": "WARRINGTON",
+                    "zip": "189762010",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }],
+            "count": 1000,
+            "business_new_entity": 923,
+            "results": [{
+                "ids": ["row_6197", "row_46760"],
+                "count": 7,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "126 W CHURCH ST",
+                    "addr2": "",
+                    "city": "DILLSBURG",
+                    "zip": "170191280",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_48339"],
+                "count": 11,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "126 W CHURCH ST",
+                    "addr2": "SUITE 200",
+                    "city": "DILLSBURG",
+                    "zip": "170191280",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_47052"],
+                "count": 9,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "20 CAPITAL DR",
+                    "addr2": "",
+                    "city": "HARRISBURG",
+                    "zip": "171109446",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_44323"],
+                "count": 16,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "429 N 21ST ST",
+                    "addr2": "",
+                    "city": "CAMP HILL",
+                    "zip": "170112202",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_47572"],
+                "count": 8,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "4665 E TRINDLE RD",
+                    "addr2": "",
+                    "city": "MECHANICSBURG",
+                    "zip": "170503640",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_45389"],
+                "count": 8,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "431 N 21ST ST",
+                    "addr2": "",
+                    "city": "CAMP HILL",
+                    "zip": "170112202",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_45941"],
+                "count": 9,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "1211 FORGE RD",
+                    "addr2": "",
+                    "city": "CARLISLE",
+                    "zip": "170133183",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }, {
+                "ids": ["row_6197", "row_47027"],
+                "count": 9,
+                "physician": {
+                    "specialty": "DIAGNOSTIC RADIOLOGY",
+                    "npi": 1124012851,
+                    "school": "ALBANY MEDICAL COLLEGE OF UNION UNIVERSITY",
+                    "first": "STEVEN",
+                    "degree": "MD",
+                    "last": "GILMAN"
+                },
+                "business": {
+                    "addr": "2313 OAKFIELD RD",
+                    "addr2": "",
+                    "city": "WARRINGTON",
+                    "zip": "189762010",
+                    "legal_name": "SPIRIT PHYSICIAN SERVICES INC"
+                }
+            }],
+            "physician_count": 1000,
+            "physician_histogram": {
+                "row_6197": 1000
+            }
+        }
+
+    return pclean_row_response(pclean_resp=pclean_resp, request=request, english_query="dummy", genfact_entity="dummy")
