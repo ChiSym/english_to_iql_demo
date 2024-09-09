0a9bfeb4b4d1170f9f0eda24a6934482cc767d1b
Schaechtle <u.schaechtle@gmail.com>
2024-09-09 17:41:37 -0400

    WIP: update model - delete four high-cardinality categoricals

--- english_to_iql_demo/run_query.py
+++ english_to_iql_demo/run_query.py
@@ -6,8 +6,7 @@ import pickle
 import geopandas as gpd
 
 from jax_multimix.interpreter import Interpreter as ProbInterpreter
-from jax_multimix.model import mixture_model, SumProductInference
-
+from jax_multimix.model import mixture_model, SumProductInference, make_GenJax_mixture, make_variables
 
 class ColumnInterpreter:
     def __init__(self, datadict, df):
@@ -25,33 +24,34 @@ class ColumnInterpreter:
 
 Interpreter = Union[ProbInterpreter, ColumnInterpreter]
 
-
 def interpreter_dispatch(grammar_path):
     if grammar_path == "us_lpm_prob.lark":
         geo_df=gpd.read_file('geodataframe.gpkg', engine='pyogrio', use_arrow=True)
         s = geo_df["geometry"].simplify(1e-2)
         geo_df["geometry"] = s
-        with open("interpreter_metadata.pkl", "rb") as f:
-            df = pl.read_parquet("synthetic_data800k.parquet", use_pyarrow=True)
-            # override state variable in synthetic data with PUMA state
-            df = df.with_columns(
-                pl.col("State_PUMA10")
-                .str.split_exact("--", 0)
-                .struct.rename_fields(["State_new"])
-                .alias("fields")
-            ).unnest("fields").select(pl.exclude('State')).rename(
-                {"State_new": "State"}
-            )
-            prob_interpreter_metadata = pickle.load(f)
-            return ProbInterpreter(
-                variables=prob_interpreter_metadata["variables"],
-                schema=json.load(open("schema.json", "r")),
-                model=mixture_model,
-                args=prob_interpreter_metadata["args"],
-                inf_alg=SumProductInference(),
-                df=df,
-                geo_df=geo_df,
-            )
+        df = pl.read_parquet("synthetic_data800k.parquet", use_pyarrow=True)
+        # override state variable in synthetic data with PUMA state
+        df = df.with_columns(
+            pl.col("State_PUMA10")
+            .str.split_exact("--", 0)
+            .struct.rename_fields(["State_new"])
+            .alias("fields")
+        ).unnest("fields").select(pl.exclude('State')).rename(
+            {"State_new": "State"}
+        )
+        with open("schema.json", "r") as f:
+            schema=json.load(f)
+        with open("lpm_parameters.json", "r") as f:
+            parameters=json.load(f)
+        return ProbInterpreter(
+            variables=make_variables(schema),
+            schema=schema,
+            model=mixture_model,
+            args=make_GenJax_mixture(parameters),
+            inf_alg=SumProductInference(),
+            df=df,
+            geo_df=geo_df,
+        )
     elif grammar_path == "us_lpm_cols.lark":
         with open("us_lpm.json", "r", encoding="utf-8") as f:
             col_interpreter_metadata = json.load(f)[0]
@@ -59,7 +59,6 @@ def interpreter_dispatch(grammar_path):
         return ColumnInterpreter(col_interpreter_metadata, df)
     else:
         raise NotImplementedError(f"Preprompt constructor not yet defined for {grammar_path}.")
-
 
 def run_query(parser, interpreter, query):
     bos, eos = " ", "\n"
